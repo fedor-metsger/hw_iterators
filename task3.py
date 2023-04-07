f72d15da6580136b82a48348c3de4cae7eb31d76
Fedor Metsger <fedor.metsger@gmail.com>
2023-04-07 11:51:47 +0300

    Initial commit

---
+++ task3.py
@@ -0,0 +1,38 @@
+
+def flat_list(lst, res):
+    [flat_list(i, res) for i in lst] if isinstance(lst, list) else res.append(lst)
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        res = []
+        flat_list(list_of_list, res)
+        self.list_of_list = iter(res)
+
+    def __iter__(self):
+        # ...
+        return self
+
+    def __next__(self):
+        item = next(self.list_of_list)
+        return item
+
+
+def test_3():
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+
+if __name__ == '__main__':
+    test_3()
---
+++ task4.py
@@ -0,0 +1,35 @@
+
+import types
+
+def flat_list(lst, res):
+    [flat_list(i, res) for i in lst] if isinstance(lst, list) else res.append(lst)
+
+def flat_generator(list_of_list):
+    res = []
+    flat_list(list_of_list, res)
+    for i in res:
+        yield i
+    # ...
+
+def test_4():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
+    assert isinstance(flat_generator(list_of_lists_2), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_4()
